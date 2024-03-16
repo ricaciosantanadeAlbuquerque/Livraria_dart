33a1275fe4e8b0e0d72b676ff165880ce7e8bfa9
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2024-03-16 17:56:38 -0300

    criando o corpo da classe Editora

---
+++ editora.dart
@@ -0,0 +1,12 @@
+class Editora {
+  String? _tipo;
+  String? _contato;
+
+  String? get tipo => this._tipo;
+
+  void set tipo(String? tipo) => this._tipo = tipo;
+
+  String? get contato => this._contato;
+
+  void set contato(String? contato) => this._contato = contato;
+}
