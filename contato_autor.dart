121a111ef20db354d6603831d05c58b5956b7539
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2024-03-16 18:39:00 -0300

    Criando o corpo da classe contato do autor

---
+++ contato_autor.dart
@@ -0,0 +1,17 @@
+class ContatoAutor {
+  String? _tipo;
+  String? _contato;
+
+  ContatoAutor({required String tipo, required String contato}) {
+    this.tipo = tipo;
+    this.contato = contato;
+  }
+
+  String? get tipo => this._tipo = tipo;
+
+  void set tipo(String? tipo) => this._tipo = tipo;
+
+  String? get contato => this._contato;
+
+  void set contato(String? contato) => this._contato = contato;
+}
