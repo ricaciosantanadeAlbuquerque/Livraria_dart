2bc8dd8071c95f7e3369b49570c494820733feef
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2024-03-16 18:01:51 -0300

    Criando o construtor da classe Editora

--- editora.dart
+++ editora.dart
@@ -2,6 +2,11 @@ class Editora {
   String? _tipo;
   String? _contato;
 
+  Editora({required String tipo, required String contato}) {
+    this.tipo = tipo;
+    this.contato = contato;
+  }
+
   String? get tipo => this._tipo;
 
   void set tipo(String? tipo) => this._tipo = tipo;
@@ -9,4 +14,6 @@ class Editora {
   String? get contato => this._contato;
 
   void set contato(String? contato) => this._contato = contato;
+
+  
 }
