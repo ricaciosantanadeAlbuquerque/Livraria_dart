5cc57c2a0591a9bda90ef5607769a4023d9ae082
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2024-03-16 23:16:08 -0300

    criando relação de composição com a classe ContatoAutor

--- autor.dart
+++ autor.dart
@@ -4,7 +4,9 @@ class Autor {
   String? _nome;
   List<ContatoAutor> _listContato = [];
 
-  Autor({required String nome, required String tipo, required String contato});
+  Autor({required String nome, required String tipo, required String contato}) {
+    criarContato(tipo: tipo, contato: contato);
+  }
 
   String? get nome => this._nome;
 
@@ -13,5 +15,18 @@ class Autor {
   List<ContatoAutor> get listContato => this._listContato;
 
   void set listContato(List<ContatoAutor> listContato) => this._listContato = listContato;
-  
+
+  void criarContato({required String tipo, required String contato}) {
+    listContato.add(ContatoAutor(tipo: tipo, contato: contato));
+  }
+
+  void removerContato() {
+    listContato.clear();
+  }
+
+  void listarContatos() {
+    for (var contato in listContato) {
+      contato.mostrarContato();
+    }
+  }
 }
