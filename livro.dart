7927e355716a0b880319751532f588677045b570
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2024-03-17 01:31:55 -0300

    get and set

--- livro.dart
+++ livro.dart
@@ -5,6 +5,39 @@ abstract class Livro {
   String? _titulo;
   int? _anoPublicacao;
   Editora? _editora;
-  List<Autor> listAutor = [];
-  
+  List<Autor> _listAutor = [];
+
+  Livro({required String titulo, required int anoPublicacao, required Editora editora}) {}
+
+  String? get titulo => this._titulo;
+
+  void set titulo(String? titulo) => this._titulo = titulo;
+
+  int? get anoPublicacao => this._anoPublicacao;
+
+  void set anoPublicacao(int? anoPublicacao) => this._anoPublicacao = anoPublicacao;
+
+  Editora? get editora => this._editora;
+
+  void set editora(Editora? editora) => this._editora = editora;
+
+  List<Autor> get listAutor => this._listAutor;
+
+  void set listAutor(List<Autor> listAutor) => this._listAutor = listAutor;
+
+  void adicionarAutor({required Autor autor}) {
+    listAutor.add(autor);
+  }
+
+  void removerAutor() {
+    listAutor.clear();
+  }
+
+  void listarAutor() {
+    for (var autor in listAutor) {
+      autor.listarContatos();
+    }
+  }
+
+
 }
